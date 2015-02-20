dde71671ca21dd008a4d5e373cdfce9ad5b6e661
Thomas Beati <thomas.beati@gmail.com>
2015-02-20 17:33:23 +0100

    Added a clock type

--- clock_windows.go
+++ clock_windows.go
@@ -5,23 +5,28 @@ package gameio
 #include <stdint.h>
 #include <Windows.h>
 
+int clockInit = 0;
 static int64_t freq;
-static int64_t base;
 
-void
-clockInit(void) {
-	QueryPerformanceFrequency((LARGE_INTEGER *)(&freq));
+int64_t
+newClock(void) {
+	if (!clockInit) {
+		QueryPerformanceFrequency((LARGE_INTEGER *)(&freq));
+		clockInit = 1;
+	}
+	int64_t base;
 	QueryPerformanceCounter((LARGE_INTEGER *)(&base));
+	return base;
 }
 
 int64_t
-clockElapsed() {
+elapsed(int64_t *base) {
 	int64_t new;
 	QueryPerformanceCounter((LARGE_INTEGER *)(&new));
 
 	int64_t elapsed;
-	elapsed = ((new - base) * 1000000000) / freq;
-	base = new;
+	elapsed = ((new - *base) * 1000000000) / freq;
+	*base = new;
 	return elapsed;
 }
 */
@@ -29,19 +34,16 @@ import "C"
 
 import "time"
 
-var initialized bool
+type Clock C.int64_t
 
-func ClockInit() {
-	C.clockInit()
-	initialized = true
+func NewClock() *Clock {
+	c := C.newClock()
+	return (*Clock)(&c)
 }
 
-func ClockElapsed() time.Duration {
-	if !initialized {
+func (c *Clock) Elapsed() time.Duration {
+	if *c == 0 {
 		panic("clock not initialized")
 	}
-
-	var elapsed time.Duration
-	elapsed = time.Duration(C.clockElapsed())
-	return elapsed
+	return time.Duration(C.elapsed((*C.int64_t)(c)))
 }
